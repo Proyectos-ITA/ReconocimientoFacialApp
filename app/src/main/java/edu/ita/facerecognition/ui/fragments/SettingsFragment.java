25560d795f398f8dd52ab8ae6c3f8f99ff842ae4
Jaen <jaennova9@gmail.com>
2023-07-20 17:16:19 -0600

    interfaz Settings fragment agregada

--- app/src/main/java/edu/ita/facerecognition/ui/fragments/SettingsFragment.java
+++ app/src/main/java/edu/ita/facerecognition/ui/fragments/SettingsFragment.java
@@ -1,66 +1,105 @@
 package edu.ita.facerecognition.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.textfield.TextInputLayout;
+
 import edu.ita.facerecognition.R;
+import edu.ita.facerecognition.domain.ServerAddress;
+import edu.ita.facerecognition.util.Utils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
+@SuppressWarnings("ConstantConditions")
+public class SettingsFragment extends CommonFragment implements View.OnClickListener {
+    public interface OnSettingsFragmentListener {
+        void onSettingsFragmentClose();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private OnSettingsFragmentListener mListener;
+    private TextInputLayout mIPTextInputLayout;
+    private TextInputLayout mPortTextInputLayout;
+
+    public SettingsFragment() { }
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.settingsOkButton).setOnClickListener(this);
+        view.findViewById(R.id.settingsCancelButton).setOnClickListener(this);
+
+        mIPTextInputLayout = view.findViewById(R.id.settingsIPTextInputLayout);
+        mPortTextInputLayout = view.findViewById(R.id.settingsPortTextInputLayout);
+
+        ServerAddress address = Utils.getServerAddress(getActivity());
+        mIPTextInputLayout.getEditText().setText(address.direccionIP);
+        mPortTextInputLayout.getEditText().setText(address.puerto);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnSettingsFragmentListener) {
+            mListener = (OnSettingsFragmentListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement OnSettingsFragmentListener");
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onBackPressed() {
+        onCloseFragment();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!validateLastClick()) return;
+
+        if (v.getId() == R.id.settingsOkButton) {
+            onSaveAddress();
+        } else if (v.getId() == R.id.settingsCancelButton) {
+            onCloseFragment();
+        }
+    }
+
+
+    private void onCloseFragment() {
+        mListener.onSettingsFragmentClose();
+    }
+
+    private void onSaveAddress() {
+        String ip = mIPTextInputLayout.getEditText().getText().toString();
+        if (!Utils.validateIP(ip, mIPTextInputLayout, getString(R.string.settings_fragment_error_ip))) {
+            return;
+        }
+
+        String port = mPortTextInputLayout.getEditText().getText().toString();
+        if (!Utils.validateText(port, mPortTextInputLayout, getString(R.string.settings_fragment_error_port))) {
+            return;
+        }
+
+        Utils.setServerAddress(getActivity(), ip, port);
+        onCloseFragment();
     }
 }
