3c0019a46268746aa33a132c7427ad81e551cc73
Jaen <jaennova9@gmail.com>
2023-07-20 17:32:11 -0600

    interfaz main fragment agregada

--- app/src/main/java/edu/ita/facerecognition/ui/fragments/MainFragment.java
+++ app/src/main/java/edu/ita/facerecognition/ui/fragments/MainFragment.java
@@ -1,66 +1,73 @@
 package edu.ita.facerecognition.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import edu.ita.facerecognition.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MainFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainFragment() {
-        // Required empty public constructor
+public class MainFragment extends CommonFragment implements View.OnClickListener {
+    public interface OnMainFragmentListener {
+        void onMainFragmentEnroll();
+        void onMainFragmentRecognize();
+        void onMainFragmentSettings();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private OnMainFragmentListener mListener;
+
+    @SuppressWarnings("WeakerAccess")
+    public MainFragment() { }
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.mainEnrollButton).setOnClickListener(this);
+        view.findViewById(R.id.mainRecognizeButton).setOnClickListener(this);
+        view.findViewById(R.id.mainSettingsButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnMainFragmentListener) {
+            mListener = (OnMainFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnMainFragmentListener");
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
+
+    @Override
+    public void onClick(View v) {
+        if (validateLastClick()) {
+            if (v.getId() == R.id.mainEnrollButton) {
+                mListener.onMainFragmentEnroll();
+            } else if (v.getId() == R.id.mainRecognizeButton) {
+                mListener.onMainFragmentRecognize();
+            } else if (v.getId() == R.id.mainSettingsButton) {
+                mListener.onMainFragmentSettings();
+            }
+        }
+    }
+
 }
